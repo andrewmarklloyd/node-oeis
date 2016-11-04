6b490f07bb6d871a3440a657fa34db91275a4245
Andrew Lloyd <andrew85.lloyd@gmail.com>
2016-11-04 14:28:02 -0700

    Implemented first tests

--- tests/test-server.js
+++ tests/test-server.js
@@ -1,17 +1,28 @@
 var chai = require('chai');
-
-
 var should = chai.should();
+var assert = require('assert');
+var expect = chai.expect();
 
+var OEIS = require('../OEIS');
+var oeis = new OEIS();
 
 
 describe('OEIS', function() {
-  it('Parses name from given html', function() {
+  describe('#getSequenceInfo', function() {
+  	it('error should be null', function(done) {
+  		oeis.getSequenceInfo(123456, function(err, info) {
+  			assert.equal(err, null);
+  			done();
+  		});
+  	});
+
+	it('should return valid OEIS sequence info', function(done) {
+  		oeis.getSequenceInfo(123456, function(err, info) {
+  			assert.equal(info.id, 'A123456');
+  			assert.equal(info.name, 'Ludwig van Beethoven, Bagatelle No. 25, \"Fuer Elise\".');
+  			done();
+  		});
+  	});
 
   });
-
-  it('Parses sequence from given html', function() {
-  	
-  });
-  
 });
