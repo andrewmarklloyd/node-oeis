3260b99771cab527b23602f5281652a1e3970a41
Andrew Lloyd <andrew85.lloyd@gmail.com>
2016-11-04 15:06:35 -0700

    Refactored tests to use before method, implemented fewer api calls

--- tests/test-server.js
+++ tests/test-server.js
@@ -1,28 +1,56 @@
 var chai = require('chai');
 var should = chai.should();
-var assert = require('assert');
+var assert = chai.assert;
 var expect = chai.expect();
+var fs = require('fs');
+
 
 var OEIS = require('../OEIS');
 var oeis = new OEIS();
 
 
 describe('OEIS', function() {
-  describe('#getSequenceInfo', function() {
-  	it('error should be null', function(done) {
-  		oeis.getSequenceInfo(123456, function(err, info) {
-  			assert.equal(err, null);
-  			done();
-  		});
-  	});
+	
+	describe('#getSequenceInfo', function() {
 
-	it('should return valid OEIS sequence info', function(done) {
-  		oeis.getSequenceInfo(123456, function(err, info) {
-  			assert.equal(info.id, 'A123456');
-  			assert.equal(info.name, 'Ludwig van Beethoven, Bagatelle No. 25, \"Fuer Elise\".');
-  			done();
-  		});
-  	});
+		var data;
+		var catchError;
 
-  });
+		before(function(done) {
+			oeis.getSequenceInfo(123456, function(err, info) {
+				data = info;
+				try {
+				oeis.getSequenceInfo("a", function(err, info) {
+					
+				});	
+			} catch(e) {
+				catchError = e;
+				done();
+			}	
+			});
+
+			
+		});
+
+		it('error should be null', function() {
+			assert.equal(data.err, null);
+		});
+
+		it('should return valid OEIS id', function() {
+			assert.equal(data.id, 'A123456');
+		});
+
+		it('should return valid OEIS name', function() {
+			assert.equal(data.name, 'Ludwig van Beethoven, Bagatelle No. 25, \"Fuer Elise\".');
+		});
+
+		it('error should be TypeError', function() {
+			assert.equal(catchError.name, 'TypeError');
+		});
+
+		it('error message should be correct', function() {
+			assert.equal(catchError.message, 'a must be a valid number');
+		});
+
+	});
 });
