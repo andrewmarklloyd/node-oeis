05783f13ede27afbd772597a6eb5e399b257ae24
Andrew Lloyd <andrew85.lloyd@gmail.com>
2016-11-06 20:45:29 -0800

    Implemented getFullSequence unit test. Added data directory to compare api returned sequence to known correct sequence

--- tests/test-server.js
+++ tests/test-server.js
@@ -13,27 +13,26 @@ describe('OEIS', function() {
 	
 	describe('#getSequenceInfo', function() {
 
+		var err;
 		var data;
 		var catchError;
 
+		// TODO: refactor to not use callback inside callback
 		before(function(done) {
 			oeis.getSequenceInfo(123456, function(err, info) {
+				err = err;
 				data = info;
 				try {
-				oeis.getSequenceInfo("a", function(err, info) {
-					
-				});	
+				oeis.getSequenceInfo("a", function(err, info) {});	
 			} catch(e) {
 				catchError = e;
 				done();
 			}	
 			});
-
-			
 		});
 
 		it('error should be null', function() {
-			assert.equal(data.err, null);
+			assert.equal(err, null);
 		});
 
 		it('should return valid OEIS id', function() {
@@ -50,6 +49,46 @@ describe('OEIS', function() {
 
 		it('error message should be correct', function() {
 			assert.equal(catchError.message, 'a must be a valid number');
+		});
+
+	});
+
+	describe('#getFullSequence', function() {
+
+		var err;
+		var data;
+		var catchError;
+
+		// TODO: refactor to not use callback inside callback
+		before(function(done) {
+			oeis.getFullSequence(54321, function(err, seq) {
+				err = err;
+				data = seq;
+				//console.log(JSON.stringify(seq));
+				try {
+					oeis.getFullSequence('b', function(err, seq) {});
+				} catch(e) {
+					catchError = e;
+					done();
+				}
+			});
+		});
+
+		it('error should be null', function() {
+			assert.equal(err, null);
+		});
+
+		it('returned sequence should be correct', function() {
+			var full_sequence = fs.readFileSync('tests/data/54321_full_sequence.json');
+			assert.deepEqual(data, JSON.parse(full_sequence));
+		});		
+
+		it('error should be TypeError', function() {
+			assert.equal(catchError.name, 'TypeError');
+		});
+
+		it('error message should be correct', function() {
+			assert.equal(catchError.message, 'b must be a valid number');
 		});
 
 	});
